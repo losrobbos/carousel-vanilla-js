7eec2416d98e32fae421d71a61c97683ec0e410e
losrobbos <losrobbos@web.de>
2023-06-29 20:32:16 +0200

    Multi step move works, but item re-arrangement still unstable

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const btnNext = document.querySelector("button.btn-next");
 const btnPrevious = document.querySelector("button.btn-previous");
 const divImages = document.querySelector("div.images");
 const icons = document.querySelector(".icons").children;
+const iconElements = Array.from(icons);
 
 // config values (grab from CSS variables)
-const config = getComputedStyle(document.documentElement)
-const imageWidth = parseInt(config.getPropertyValue("--image-width"))
-const imageHeight = parseInt(config.getPropertyValue("--image-height"))
-const transition = config.getPropertyValue("--transition")
-const transitionInt = parseInt(config.getPropertyValue("--transition-number")) * 1100
+const config = getComputedStyle(document.documentElement);
+const imageWidth = parseInt(config.getPropertyValue("--image-width"));
+const imageHeight = parseInt(config.getPropertyValue("--image-height"));
+const transition = config.getPropertyValue("--transition");
+const transitionInt =
+  parseInt(config.getPropertyValue("--transition-number")) * 1100;
 
 let currentIndex = 0;
-let blockButtons = false
+let blockButtons = false;
 
 const images = [
   `./images/ananas.resized.jpg`,
@@ -37,61 +39,83 @@ const imgElements = images.map((img, i) => {
   return imgElement;
 });
 
-// create icon handlers 
-Array.from(icons).forEach((icon, i) => {
+// create icon handlers
+iconElements.forEach((icon, i) => {
   icon.addEventListener("click", () => {
-    const newIndex = i
-    console.log("We move img from ", currentIndex, "to", newIndex)
+    const newIndex = i;
     const diff = newIndex - currentIndex;
-    console.log("Diff: ", diff);
-    // cases: 
+    console.log({ currentIndex, newIndex, diff });
+    // cases:
     // if 0 => we do nothing
+    // if from LAST to FIRST or vice versa => we move just ONE!
     // if -1 => we move one BACK
-    // if something else => we move FORWARD
-    if(diff === 0) return
-    if(diff === -1) 
-      slidePrevious()
-    if(diff === 1) {
-      slideNext()
+    // if 1 => we move one FORWARD
+    // if something else => we move X FORWARD
+    if (diff === 0) return;
+
+    // from first to last => go one back
+    if (diff > 0 && diff === imgElements.length - 1) {
+      slidePrevious();
     }
-    // move "diff" steps forward (!)
-    console.log("Slide multiple!")
-    slideNext()
-    slideNext()
-  })
-})
+    // from last to first => go one forward
+    if (diff < 0 && Math.abs(diff) === imgElements.length - 1) {
+      slideNext();
+    }
+
+    if (diff === -1) slidePrevious();
+    if (diff === 1) {
+      slideNext();
+    }
+    // move "diff" steps
+    if(diff > 0) {
+      console.log(diff, "forwards")
+      slideNext(diff);
+    }
+    else {
+      console.log(diff, "back");
+      slidePrevious(Math.abs(diff))
+    }
+  });
+});
 
 const setIndex = (inc) => {
-  currentIndex += inc
-  if(currentIndex < 0) {
-    currentIndex = imgElements.length -1
+  currentIndex += inc;
+  if (currentIndex < 0) {
+    currentIndex = imgElements.length - 1;
+  } else if (currentIndex === imgElements.length) {
+    currentIndex = 0;
   }
-  else if(currentIndex === imgElements.length) {
-    currentIndex = 0
-  }
-}
+  // color selected icon (at index position)
+  iconElements.forEach((icon, i) => {
+    if (i === currentIndex) {
+      return icon.classList.add("active");
+    }
+    icon.classList.remove("active");
+  });
+};
 
 // click LEFT
-const slidePrevious = () => {
+const slidePrevious = (inc = 1) => {
   if (blockButtons) return;
   blockButtons = true;
 
-  setIndex(-1)
-  console.log("New position:", currentIndex);
+  setIndex(-inc);
 
-  // move ALL images one image width to the RIGHT (except LAST item)
+  // move ALL images X image widths to the RIGHT
   imgElements.forEach((img, i) => {
     const { left } = getComputedStyle(img);
-    const leftNew = parseInt(left) + imageWidth + "px";
-    img.style.left = leftNew
+    const leftNew = parseInt(left) + imageWidth * inc + "px";
+    img.style.left = leftNew;
   });
-  // move LAST image to FIRST position in container
+
+  // move LAST X images to FIRST position in container
   const lastItem = imgElements[imgElements.length - 1];
-  lastItem.style.zIndex = -1
-  const leftNew = -imageWidth + "px"
+  // lastItem.style.zIndex = -1
+  lastItem.style.display = "none";
+  const leftNew = -imageWidth + "px";
   lastItem.style.left = leftNew;
 
-  resetItem(lastItem)
+  resetItem(lastItem);
 
   // move element to FIRST position in array
   const firstItemNew = imgElements.pop(); // remove last array item
@@ -99,25 +123,26 @@ const slidePrevious = () => {
 };
 
 // click RIGHT
-const slideNext = () => {
+const slideNext = (inc = 1) => {
   if (blockButtons) return;
   blockButtons = true;
 
-  setIndex(1)
-  console.log("New position:", currentIndex)
+  setIndex(inc);
 
-  // move ALL images one image width to the LEFT
+  // move ALL images X image widths to the LEFT
   imgElements.forEach((img, i) => {
     const { left } = getComputedStyle(img);
-    img.style.left = parseInt(left) - imageWidth + "px";
+    img.style.left = parseInt(left) - imageWidth * inc + "px";
   });
 
-  // move FIRST item to last position in container
+  // move FIRST X items to last position in container (AFTER move left is finished!)
   const firstItem = imgElements[0];
-  firstItem.style.zIndex = -1
-  firstItem.style.left = (imgElements.length - 2) * imageWidth + "px";
+  firstItem.style.display = "none";
+  // firstItem.style.zIndex = -1
+  const newLeft = (imgElements.length - 2 - inc+1) * imageWidth + "px";
+  firstItem.style.left = newLeft;
 
-  resetItem(firstItem)
+  resetItem(firstItem);
 
   // move element to LAST position in array
   const lastItemNew = imgElements.shift();
@@ -128,14 +153,15 @@ const slideNext = () => {
 const resetItem = (item) => {
   // allow user to slide again in round about a second
   setTimeout(() => {
-    item.style.removeProperty("z-index");
+    item.style.display = "grid";
+    // item.style.removeProperty("z-index");
     blockButtons = false;
   }, transitionInt);
 };
 
 // make buttons work...
-btnPrevious.addEventListener("click", slidePrevious);
-btnNext.addEventListener("click", slideNext);
+btnPrevious.addEventListener("click", () => slidePrevious(1));
+btnNext.addEventListener("click", () => slideNext(1));
 
 // autoslide...
 // setInterval(() => {
